0db8ae644d7104c1772629b12455bd95f9efccee
ERJ92 <emmanuelr610@gmail.com>
2021-02-15 00:28:12 -0600

    cambios

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'Hello World'
-    
+    return render_template('index.html')
+
+@app.route('/Acerca')
+def about():
+    return 'About Page'
+if __name__ == '__main__':
+    app.run()
